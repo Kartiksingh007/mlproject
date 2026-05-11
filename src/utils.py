2dc2286ed13fa9e32047afb4c2dd1543bac7b7a1
Kartikey Rajput <kr2637804@gmail.com>
2026-05-12 01:16:12 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -3,12 +3,16 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.model_selection import train_test_split
+
+from sklearn.metrics import r2_score
 
 
 from src.exception import CustomException
 
 def save_obj(file_path,obj):
     try :
+
         dir_path = os.path.dirname(file_path)
 
         os.makedirs(dir_path,exist_ok=True)
@@ -18,3 +22,31 @@ def save_obj(file_path,obj):
 
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+
+        for i in range (len(list(models))):
+            model_name=list(models.keys())[i]
+            model=list(models.values())[i]
+
+            model.fit(X_train,y_train) # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train,y_train_pred)
+
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+        
+    except Exception as e:
+        raise CustomException(e,sys)
+ 
+        
